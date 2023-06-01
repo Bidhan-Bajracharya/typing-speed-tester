83d72a357fdd3ec4112b64aa91870de1f37ab701
Bidhan Bajracharya <95129959+Bidhan-Bajracharya@users.noreply.github.com>
2023-06-01 19:33:51 +0545

    Working on layouts

---
+++ src/components/UI/SentenceContainer.tsx
@@ -0,0 +1,20 @@
+const SentenceContainer = () => {
+  return (
+    <>
+      <div className="bg-sb px-9 py-6 w-full h-[77px] rounded-md overflow-clip">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        imperdiet tristique ex. Nunc vitae lacinia augue. Nunc pharetra egestas
+        sapien a mattis. Curabitur sodales purus a odio accumsan, et placerat
+        diam consequat. Duis elit turpis, dignissim at sapien eget, maximus
+        sagittis ipsum. Aenean tincidunt luctus nunc vel semper. Nam pulvinar
+        gravida hendrerit. Quisque lobortis aliquam felis eget pretium. Praesent
+        ornare ligula posuere elit hendrerit, eu semper nulla aliquam. Fusce leo
+        eros, ultrices non leo in, accumsan tincidunt mi. Etiam aliquet vitae
+        risus sed consectetur. Fusce quis porta sem, suscipit dignissim est. Sed
+        non aliquam est, sit amet dignissim turpis.
+      </div>
+    </>
+  );
+};
+
+export default SentenceContainer;
--- src/components/layout/Body.tsx
+++ src/components/layout/Body.tsx
@@ -1,25 +1,51 @@
+import MenuItem from "@mui/material/MenuItem";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import SentenceContainer from "../UI/SentenceContainer";
+import TextField from "@mui/material/TextField";
+
 const Body = () => {
   return (
     <>
-        <main className="w-full h-screen font-poppins flex flex-col items-center bg-tb text-white">
-            {/* choices section*/}
-            <section className="border-yellow-500 border-2">
-                <h1>Test Your Limit</h1>
+      <main className="w-full h-screen font-poppins flex flex-col items-center bg-tb text-white">
+        {/* choices section*/}
+        <section className="border-yellow-500 border-2 mt-16">
+          <h1 className="text-3xl">Test Your Limit</h1>
 
-            </section>
+          <div className="flex flex-row mt-[20px]">
+            <Select className="bg-sb border-red-400" label="Duration">
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </div>
+        </section>
 
-            {/* typing section */}
-            <section className="border-green-500 border-2">
-                Type here
-            </section>
-            
-            {/* result section */}
-            <section className="border-purple-500 border-2">
-                Results
-            </section>
-        </main> 
+        {/* typing section */}
+        <section className="border-green-500 border-2 mt-12 w-[815px] h-[214px]">
+          <SentenceContainer />
+
+          <div className="flex flex-row">
+            <input className="w-[50%]"/>
+
+            <div className="bg-sb flex p-5 justify-center items-center rounded-md m-[0.5rem]">
+                WPM
+            </div>
+
+            <div className="bg-sb flex p-5 justify-center items-center rounded-md m-[0.5rem]">
+                Timer
+            </div>
+
+            <div className="bg-sb flex p-5 justify-center items-center rounded-md m-[0.5rem]">
+                Reset
+            </div>
+          </div>
+        </section>
+
+        {/* result section */}
+        <section className="border-purple-500 border-2">Results</section>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
