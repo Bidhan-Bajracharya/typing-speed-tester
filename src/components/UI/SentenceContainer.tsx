503b3e800cb639bacbaf778280f5df19886d349e
Bidhan Bajracharya <95129959+Bidhan-Bajracharya@users.noreply.github.com>
2023-06-02 17:05:27 +0545

    Adding color co-ordination logic

--- src/components/UI/SentenceContainer.tsx
+++ src/components/UI/SentenceContainer.tsx
@@ -1,17 +1,34 @@
-const SentenceContainer = () => {
+import React from "react";
+
+interface Word {
+  word: string;
+  correct: string;
+}
+interface ChallengeWords {
+  words: Word[];
+}
+
+const SentenceContainer: React.FC<ChallengeWords> = ({ words }) => {
   return (
     <>
       <div className="bg-sb px-9 py-6 w-full h-[77px] rounded-md overflow-clip">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        imperdiet tristique ex. Nunc vitae lacinia augue. Nunc pharetra egestas
-        sapien a mattis. Curabitur sodales purus a odio accumsan, et placerat
-        diam consequat. Duis elit turpis, dignissim at sapien eget, maximus
-        sagittis ipsum. Aenean tincidunt luctus nunc vel semper. Nam pulvinar
-        gravida hendrerit. Quisque lobortis aliquam felis eget pretium. Praesent
-        ornare ligula posuere elit hendrerit, eu semper nulla aliquam. Fusce leo
-        eros, ultrices non leo in, accumsan tincidunt mi. Etiam aliquet vitae
-        risus sed consectetur. Fusce quis porta sem, suscipit dignissim est. Sed
-        non aliquam est, sit amet dignissim turpis.
+        {words.map((word) => (
+          <span
+            key={word.word}
+            // className={`${
+            //   word.correct == "pending"
+            //     ? "text-white"
+            //     : word.correct == "correct"
+            //     ? "text-green-600"
+            //     : "text-red-500"
+            // }`}
+            className={`${word.correct == "pending" && "text-white"} ${
+              word.correct == "correct" && "text-green-600"
+            } ${word.correct == "wrong" && "text-red-600"}`}
+          >
+            {word.word + " "}
+          </span>
+        ))}
       </div>
     </>
   );
--- src/components/layout/Body.tsx
+++ src/components/layout/Body.tsx
@@ -1,9 +1,65 @@
-import MenuItem from "@mui/material/MenuItem";
+import { useState, KeyboardEvent } from "react";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import SentenceContainer from "../UI/SentenceContainer";
-import TextField from "@mui/material/TextField";
 
 const Body = () => {
+  const [words, setWords] = useState([
+    { word: "apple", correct: "pending" },
+    { word: "banana", correct: "pending" },
+    { word: "orange", correct: "pending" },
+  ]);
+
+  // number of words typed by user
+  const [spaceCounter, setSpaceCounter] = useState(0);
+
+  // tracking the word typed by the user
+  const [userWord, setUserWord] = useState("");
+
+  const handleWordSubmit = (word: string) => {
+    setUserWord(word);
+  };
+
+  // checking if user-typed word matched existing word
+  const handleWordCheck = (status: string) => {
+    setWords((prevWords) => {
+      const updatedWords = prevWords.map((word) => {
+        if (word.word === userWord) {
+          return { ...word, correct: `${status}` };
+        }
+        return word;
+      });
+      return updatedWords;
+    });
+  }
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    // check for spacebar
+    if (event.key === " " || event.key === "Spacebar") {
+      const target = event.target as HTMLTextAreaElement;
+      
+      event.preventDefault(); // Prevents the space character from being entered in the input field
+      setUserWord(target.value.trim());
+      console.log("before" + spaceCounter);
+      
+      // check if the entered word matched the current word
+      if (userWord === words[spaceCounter].word) {
+        handleWordCheck("correct");
+        console.log("right");
+      } else if(userWord !== words[spaceCounter].word){
+        handleWordCheck("wrong");
+        console.log("false");
+      }
+      
+      setSpaceCounter((prevState) => prevState + 1);
+      setUserWord("");
+      console.log("after" + spaceCounter);
+      
+    }
+  };
+
+  console.log(words);
+  
+
   return (
     <>
       <main className="w-full h-screen font-poppins flex flex-col items-center bg-tb text-white">
@@ -12,31 +68,36 @@ const Body = () => {
           <h1 className="text-3xl">Test Your Limit</h1>
 
           <div className="flex flex-row mt-[20px]">
-            <Select className="bg-sb border-red-400" label="Duration">
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
-            </Select>
+            {/* <Select className="bg-sb border-red-400" label="Duration">
+              <MenuItem value={1}>Ten</MenuItem>
+              <MenuItem value={2}>Twenty</MenuItem>
+              <MenuItem value={3}>Thirty</MenuItem>
+            </Select> */}
           </div>
         </section>
 
         {/* typing section */}
         <section className="border-green-500 border-2 mt-12 w-[815px] h-[214px]">
-          <SentenceContainer />
+          <SentenceContainer words={words} />
 
           <div className="flex flex-row">
-            <input className="w-[50%]"/>
+            <input
+              className="w-[50%] text-black m-[0.5rem] rounded-sm p-3"
+              value={userWord}
+              onKeyDown={handleKeyDown}
+              onChange={(e) => handleWordSubmit(e.target.value)}
+            />
 
-            <div className="bg-sb flex p-5 justify-center items-center rounded-md m-[0.5rem]">
-                WPM
+            <div className="bg-sb flex p-5 justify-center items-center grow rounded-md m-[0.5rem]">
+              WPM
             </div>
 
-            <div className="bg-sb flex p-5 justify-center items-center rounded-md m-[0.5rem]">
-                Timer
+            <div className="bg-sb flex p-5 justify-center items-center grow rounded-md m-[0.5rem]">
+              Timer
             </div>
 
-            <div className="bg-sb flex p-5 justify-center items-center rounded-md m-[0.5rem]">
-                Reset
+            <div className="bg-sb flex p-5 justify-center items-center grow rounded-md m-[0.5rem] cursor-pointer">
+              Reset
             </div>
           </div>
         </section>
